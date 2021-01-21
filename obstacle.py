d23dc3be521c64b277f364551a6dcdafc0c13037
EthanU <71835453+EthanU@users.noreply.github.com>
2021-01-21 11:12:39 -0600

    Add files via upload

--- obstacle.py
+++ obstacle.py
@@ -4,7 +4,7 @@ class Obstacle(object):
 	"""docstring for Obstacle"""
 	def __init__(self, image_name, x, y):
 		super(Obstacle, self).__init__()
-		self.image = pygame.image.load(image_name)
+		self.image = pygame.image.load(image_name).convert_alpha()
 		self.image = pygame.transform.scale(self.image, (50, 50))
 		self.x = x
 		self.y = y
